2ca5c091efecdcf782dc5d2a2d8c9d1227e8e4d0
RincLiu <i@rincliu.com>
2013-10-31 22:59:05 +0800

    添加ROOT管理模块

---
+++ src/com/rincliu/library/common/persistence/rootmanager/exception/PermissionException.java
@@ -0,0 +1,16 @@
+
+package com.rincliu.library.common.persistence.rootmanager.exception;
+
+/**
+ * This exception will be throwed when Root permission error occurs.
+ * 
+ * @author Chris Jiang
+ */
+public class PermissionException extends Exception {
+
+    private static final long serialVersionUID = -8713947214162841310L;
+
+    public PermissionException(String error) {
+        super(error);
+    }
+}
